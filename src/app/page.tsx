26222e9c37fb2178f41672f87c1c6a72432fe624
ankur-originhealthai <ankurbaijal@originmedical.ai>
2025-05-21 17:12:01 +0530

    ADD: Recording buttons and css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,14 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="items-center justify-items-center min-h-screen p-8 pb-2 gap-1 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    
+        <h2>Welcome to Radiology Department</h2>
+        <h4>We wish you a healthy baby !!</h4>
         <button className="m-2 p-2 bg-blue-400 rounded-2xl cursor-pointer" onClick={() => router.push("/ultrasound_video")}> Start Ultrasound</button>
 
         
-      </main>
+
     </div>
   );
 }
--- src/app/ultrasound_video/page.tsx
+++ src/app/ultrasound_video/page.tsx
@@ -1,19 +1,48 @@
-"use client"
-import {useState} from "react"
+"use client";
+import Video from "next-video"
+import { useState } from "react";
 
 const Ultrasound_video = () => {
-    const [startButton, setStartButton] = useState <String>("Start Recoring")
+  const [startButton, setStartButton] = useState<String>("Start Recoring");
+  const [endButton, setEndButton] = useState<Boolean>(false);
+  const handleRecording = () => {
+    setStartButton("Recoridng in Progress");
+    setEndButton(true);
+  };
+  const handleEndRecoring = () => {
+    setStartButton("StartRecording");
+    setEndButton(false);
+  };
   return (
-    <div className="bg-black">
-      <div>
-        <h2>Let's start your Ultrasound</h2>
+    <div>
+      <div className="bg-black">
+        <div>
+          <h2 className="text-shadow-amber-200">Let's start your Ultrasound</h2>
+        </div>
+        <div className="flex mx-4 justify-center items-center">
+          <video autoPlay muted loop>
+            <source src="/ultrasoundvideo.mp4" type="video/mp4"></source>
+          </video>
+        </div>
       </div>
-      <div className="flex mx-4 justify-center items-center">
-        <video width="520" height="440" autoPlay muted loop>
-          <source src="/ultrasoundvideo.mp4" type="video/mp4"></source>
-        </video>
+
+      <div className="flex justify-center items-center">
+        <button
+          className="m-2 p-2 bg-red-400 rounded-2xl cursor-pointer text-white font-bold "
+          onClick={handleRecording}
+        >
+          {startButton}
+        </button>
+
+        {endButton && (
+          <button
+            className="m-2 p-2 bg-blue-400 rounded-2xl cursor-pointer text-white font-bold"
+            onClick={handleEndRecoring}
+          >
+            Stop Recording
+          </button>
+        )}
       </div>
-      
     </div>
   );
 };
